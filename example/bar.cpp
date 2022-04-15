882339dc439b85bddaba8abadbd14b00a16e0630
Tomomi_Murakami <tomomi.murakami1225@gmail.com>
2022-04-16 01:36:32 +0900

    exampleをビルド対象に追加

--- example/bar.cpp
+++ example/bar.cpp
@@ -1,19 +1,9 @@
 ﻿#include <cstdio>
-#include <labdog/logging/log.hpp>
-#include <labdog/util/assert.hpp>
 #include <labdog/core/string.hpp>
-#include <labdog/core/string_view.hpp>
 
 int main(int argc, char *argv[])
 {
-	labdog::string str(U"☺");
-	labdog::string str2(U"😂あいうえ");
-	labdog::string_view view(str);
-	labdog::string_view view2(str2.begin(), str2.begin() + 2);
-
-	bool res = view == view2;
-
-	getchar();
+	ld::string str(U"labdog🐶🦴");
 
 	return 0;
 }
