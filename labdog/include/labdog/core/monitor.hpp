250f186889e4f12550deb55af7df69774c7e16be
Tomomi_Murakami <tomomi.murakami1225@gmail.com>
2022-05-17 09:35:37 +0900

    Create monitor.hpp

---
+++ labdog/include/labdog/core/monitor.hpp
@@ -0,0 +1,22 @@
+//=========================================================
+//
+//  monitor.hpp is part of the labdog project.
+//  Copyright (C) 2022 TarobeWanwanLand.
+//
+//  Released under the MIT license.
+//  see http://opensource.org/licenses/MIT
+//
+//=========================================================
+
+#ifndef LABDOG_MONITOR_HPP
+#define LABDOG_MONITOR_HPP
+
+namespace ld
+{
+    class Monitor
+    {
+
+    };
+}
+
+#endif // LABDOG_MONITOR_HPP
